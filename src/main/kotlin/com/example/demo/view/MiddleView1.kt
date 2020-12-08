b6db88ace8fa6b5df0cc89daf7e88bac1f966513
jay_wu <jay_wu@gss.com.tw>
2020-12-08 16:58:09 +0800

    -

---
+++ src/main/kotlin/com/example/demo/view/MiddleView1.kt
@@ -0,0 +1,7 @@
+package com.example.demo.view
+
+import tornadofx.*
+
+class MiddleView1: View() {
+    override val root = label("Middle 1")
+}
---
+++ src/main/kotlin/com/example/demo/view/MiddleView2.kt
@@ -0,0 +1,7 @@
+package com.example.demo.view
+
+import tornadofx.*
+
+class MiddleView2: View() {
+    override val root = label("Middle 2")
+}
