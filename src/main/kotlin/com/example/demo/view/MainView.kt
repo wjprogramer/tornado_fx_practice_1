e68dda7207b233288faf3d18a2a20adbe98094a8
jay_wu <jay_wu@gss.com.tw>
2020-12-08 16:58:21 +0800

    Injection Using find() or inject()

--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -4,8 +4,16 @@ import com.example.demo.app.Styles
 import tornadofx.*
 
 class MainView : View("Hello TornadoFX") {
+
+    // Explicitly retrieve TopView
+    private val middleView1 = find(MiddleView1::class)
+    // Create a lazy reference to BottomView
+    private val middleView2: MiddleView2 by inject()
+
     override val root = borderpane  {
         top<TopView>()
+        middleView1.root
+        middleView2.root
         bottom<BottomView>()
     }
 }
