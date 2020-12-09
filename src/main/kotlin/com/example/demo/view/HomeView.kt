323466d9f2d31257974d0c0b783ad3dfac9a54d5
jay_wu <jay_wu@gss.com.tw>
2020-12-09 09:18:49 +0800

    dock

--- src/main/kotlin/com/example/demo/view/HomeView.kt
+++ src/main/kotlin/com/example/demo/view/HomeView.kt
@@ -6,4 +6,16 @@ class HomeView : View("Home View") {
     override val root = vbox {
         label("HomeView")
     }
+
+    // View 的 root 與 Parent connected 的時候
+    override fun onDock() {
+        super.onDock()
+        println("Docking HomeView!")
+    }
+
+    // ... disconnected ...
+    override fun onUndock() {
+        super.onUndock()
+        println("Undocking HomeView!")
+    }
 }
