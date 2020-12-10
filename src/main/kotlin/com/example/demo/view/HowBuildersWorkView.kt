2186d358c66aded94ee26a155787bccc7ca131af
jay_wu <jay_wu@gss.com.tw>
2020-12-10 14:37:15 +0800

    builder work

--- src/main/kotlin/com/example/demo/view/HowBuildersWorkView.kt
+++ src/main/kotlin/com/example/demo/view/HowBuildersWorkView.kt
@@ -2,6 +2,7 @@ package com.example.demo.view
 
 import javafx.scene.control.Button
 import javafx.scene.layout.VBox
+import javafx.scene.paint.Color
 import tornadofx.*
 
 // 解釋用的 View，不是好的寫法
@@ -10,8 +11,24 @@ class HowBuildersWorkView : View("My View") {
     override val root = VBox()
 
     init {
-        with(root) {
+        /** 1. */
+//        with(root) {
+//            this += Button("Press Me")
+//
+//            button("Press Me") {
+//                textFill = Color.RED
+//                action { println("Button pressed!") }
+//            }
+//        }
+
+        /** 2. */
+        root.apply {
             this += Button("Press Me")
+
+            this += Button("Press Me").apply {
+                textFill = Color.RED
+                action { println("Button pressed!") }
+            }
         }
     }
 
